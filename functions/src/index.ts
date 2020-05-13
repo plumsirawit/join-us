43d62b47f71830cd16e730adf86fb3a14903b3e8
Plum <sirawit@brikl.com>
2020-05-14 01:24:24 +0700

    finish up the main function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,60 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
+import * as mime from "mime-types";
 
-import * as express from 'express'
-import * as multer from 'multer'
-import * as cors from 'cors'
-const upload = multer();
+import * as express from "express";
+//@ts-ignore
+import * as fileMiddleware from "express-multipart-file-parser";
+import * as cors from "cors";
 
 const app = express();
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
+app.use(fileMiddleware);
 
-app.post('/', upload.single('file'), (req, res) => {
-    console.log(req.file);
-    console.log(req.body);
+admin.initializeApp();
+const db = admin.firestore();
+const bucket = admin.storage().bucket();
+
+app.post("/", (req, res) => {
+    //@ts-ignore
+    const file = req.files[0];
     const inpObj = req.body;
-    //const file = req.file;
-    if(!inpObj.email || inpObj.email.length > 128) res.status(400).send('Bad Request');
-    if(!inpObj.name || inpObj.name.length > 32) res.status(400).send('Bad Request');
-    if(!inpObj.statement || inpObj.statement.length > 1000) res.status(400).send('Bad Request');
-    //const {email, name, statement} = inpObj;
-    //console.log(email, name, statement, file);
-    res.status(200).send('OK');
+    if (!inpObj.email || inpObj.email.length > 128)
+        return res.status(400).send("Bad Request");
+    else if (!inpObj.name || inpObj.name.length > 32)
+        return res.status(400).send("Bad Request");
+    else if (!inpObj.statement || inpObj.statement.length > 1000)
+        return res.status(400).send("Bad Request");
+    else {
+        const { email, name, statement } = inpObj;
+        return db
+            .collection("applications")
+            .add({
+                email,
+                name,
+                statement,
+            })
+            .then((ref) => {
+                console.log(ref.id);
+                console.log(file.originalname);
+                const ext = mime.extension(file.mimetype);
+                const fpath = path.join(os.tmpdir(), ref.id + "." + ext);
+                return new Promise<string>((resolve) => {
+                    fs.writeFile(fpath, file.buffer, () => resolve(fpath));
+                });
+            })
+            .then((fpath) => {
+                return bucket.upload(fpath);
+            })
+            .then(() => {
+                return res.status(200).send("OK");
+            });
+    }
 });
 
-export const submitApplication = functions.region("asia-east2").https.onRequest(app);
+export const submitApplication = functions
+    .region("asia-east2")
+    .https.onRequest(app);
